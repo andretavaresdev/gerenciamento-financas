25e1027c8d47bd7c6a40b234a090ee44b2bd0f6c
Andre Tavares <andreetavaress1@gmail.com>
2026-03-25 19:49:39 -0300

    add: pastas iniciais, firstCommit

---
+++ src/main/java/com/tavares/gerenciamentofinancas/GerenciamentofinancasApplication.java
@@ -0,0 +1,13 @@
+package com.tavares.gerenciamentofinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentofinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentofinancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tavares/gerenciamentofinancas/GerenciamentofinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tavares.gerenciamentofinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentofinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
